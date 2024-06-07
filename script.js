dbb4e6ecb0c429ef0328d0d475dad4082e398f1a
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-07 19:39:41 +0400

    get data, get usernames and colors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const ulEl = document.getElementById("ul");
 const request = new XMLHttpRequest();
 
 request.open("GET", "https://reqres.in/api/unknown");
@@ -5,7 +6,13 @@ request.open("GET", "https://reqres.in/api/unknown");
 request.addEventListener("load", function () {
   const responseText = this.responseText;
   const responseJs = JSON.parse(responseText);
-  console.log(responseJs.data);
+  const arrayUsers = responseJs.data;
+  arrayUsers.forEach((element) => {
+    const li = document.createElement("li");
+    li.textContent = element.name;
+    li.style.backgroundColor = element.color;
+    ulEl.appendChild(li);
+  });
 });
 
 request.send();
